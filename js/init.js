e98601a65108c9521ba029fe0bac009afd9b1f34
Nicholas A Ciccarino <nciccarino@gmail.com>
2018-07-01 19:12:05 -0400

    slider

--- js/init.js
+++ js/init.js
@@ -4,5 +4,16 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+  	// Or with jQuery
+
+  	$(document).ready(function(){
+   	 	$('.slider').slider();
+  	});
+
+  	$(document).ready(function(){
+    	$('.materialboxed').materialbox();
+  	});
+        
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
